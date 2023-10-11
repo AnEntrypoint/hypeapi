e5e2d5a747942d40f229490cd222f306581faaac
lanmower <almagestfraternite@gmail.com>
2023-10-11 16:47:34 +0000

    update

--- server.js
+++ server.js
@@ -33,14 +33,6 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-app.use((function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "https://svelvet.lan.247420.xyz")
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type")
-  res.setHeader("Access-Control-Allow-Credentials", true)
-  next();
-}));
-
 app.use("/task", tasks(node, db))
 app.use("/run", call(node))
 
